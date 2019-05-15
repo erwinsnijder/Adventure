7d59e7f07c1dfd62faae45cf39e16a4b156d08d6
erwin snijder <erwinsnijder_4@hotmail.com>
2019-05-15 16:46:55 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var entercabin = confirm("You come across a strange house in the woods,do you want to go inside?");
+alert (entercabin);
+
+if (entercabin == true) {
+
+   alert("you decide to enter the cabin and find some delicious bowls of oatmeal");
+}
+   else {
+   alert ("you don't go in the cabin so this adventure is over");
+}
