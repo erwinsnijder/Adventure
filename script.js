6c8b44c884c15d0fbaf1e5822249a56cee817d81
erwin snijder <erwinsnijder_4@hotmail.com>
2019-05-17 09:54:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var entercabin = confirm("You come across a strange house in the woods,do you want to go inside?");
-alert (entercabin);
 
 if (entercabin == true) {
 
-   alert("you decide to enter the cabin and find some delicious bowls of oatmeal");
-}
-   else {
+   var porridgechoice = prompt("you decide to enter the cabin and find some delicious bowls of oatmeal, do you try bowl 1, 2 or 3?");
+    
+   if (porridgechoice == 1) {
+alert("this one is to hot! the end.")
+
+   }
+   else if (porridgechoice == 2){
+alert("this one is to cold! the end.")
+   }
+   else  {
+      alert("this one is justt right!")
+
+
+   }
+   }
+else {
    alert ("you don't go in the cabin so this adventure is over");
+   
 }
